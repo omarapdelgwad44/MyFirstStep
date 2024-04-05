83c6ee56a9c701f653431dc1455d44859e34e1d6
omarapdelgwad44 <96298796+omarapdelgwad44@users.noreply.github.com>
2024-04-06 00:01:48 +0200

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome");
+            while (true)
+            {
+                Console.WriteLine("-----------------------------------------");
+                Console.WriteLine("1- Add User");
+                Console.WriteLine("2- Edit User");
+                Console.WriteLine("3-Delete User");
+                Console.WriteLine("4-Show");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        {
+                            Console.WriteLine("Enter The User Data");
+                            Console.Write("Name:");
+                            string name = Console.ReadLine();
+                            Console.Write("PhoneNumber:");
+                            string phonenumber = Console.ReadLine();
+                            Console.Write("Email:");
+                            string email = Console.ReadLine();
+                            User user = new User(name, email, phonenumber);
+                            Console.WriteLine("Done");
+                            break;
+                        }
+                    case "2":
+                        {
+                            Console.WriteLine("Enter The User PhoneNumber ");
+                            string phonenumber = Console.ReadLine();
+                            var user = User.Search(phonenumber);
+                            if (user != null)
+                            {
+                                Console.WriteLine("What Do You Want To Edit");
+                                Console.WriteLine("1- Name");
+                                Console.WriteLine("2- Email");
+                                Console.WriteLine("3- PhoneNumber");
+                                switch (Console.ReadLine())
+                                {
+                                    case "1":
+                                        Console.Write("New Name:");
+                                        string name = Console.ReadLine();
+                                        User.EditName(user, name);
+                                        Console.WriteLine("Done");
+                                        break;
+                                    case "2":
+                                        Console.Write("New Email:");
+                                        string email = Console.ReadLine();
+                                        User.EditEmail(user, email);
+                                        Console.WriteLine("Done");
+                                        break;
+                                    case "3":
+                                        Console.Write("New PhoneNumber:");
+                                        string phoneNumber = Console.ReadLine();
+                                        User.EditPhone(user, phoneNumber);
+                                        Console.WriteLine("Done");
+                                        break;
+                                    default:
+                                        Console.WriteLine("invalid option");
+                                        break;
+                                }
+                            }
+                            break;
+                        }
+                    case "3":
+                        {
+                            Console.WriteLine("Enter The User PhoneNumber ");
+                            string phonenumber = Console.ReadLine();
+                            var user = User.Search(phonenumber);
+                            if (user != null)
+                            {
+                                User.Delete(user);
+                                Console.WriteLine("Done");
+                            }
+                            break;
+                        }
+                    case "4":
+                        {
+                            Console.WriteLine("-----------------------------------------");
+                            User.show();
+                            break;
+                        }
+                    default:
+                        Console.WriteLine("invalid Option");
+                        break;
+                }
+                Console.WriteLine("Again??");
+                if (Console.ReadLine().ToLower() !="y")break;
+            }
+            Console.WriteLine("thanks ;) ");
+        }
+    }
+}
---
+++ ConsoleApp4/User.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class User
+    {
+       static private List<User> users = new List<User>();
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public User(string name,string email,string phonenumber)
+        {
+            Name = name;
+            Email = email;
+            PhoneNumber = phonenumber;
+            users.Add(this);
+        }
+       static public User Search (string phonenumber)
+        {
+            foreach (var user in users)
+            {
+               if( user.PhoneNumber == phonenumber)
+                    return user;
+            }
+            Console.WriteLine("invalied User");
+            return null;
+        }
+        
+        static public void EditPhone(User user,string phonenumber)
+        {
+            user.PhoneNumber = phonenumber;
+        }
+        static public void EditName(User user, string name)
+        {
+            user.Name = name;
+        }
+        static public void EditEmail(User user, string email)
+        {
+            user.Email = email;
+        }
+        static public void Delete(User user)
+        {
+            users.Remove(user);
+        }
+        public static void show() {
+            if(users.Count == 0) { Console.WriteLine("We dont have users");return;}
+            foreach (var user in users)
+            {
+                Console.WriteLine(user.Name);
+                Console.WriteLine(user.Email);
+                Console.WriteLine(user.PhoneNumber);
+                Console.WriteLine("-----------------------------------------");
+            }
+        }
+    }
+}
